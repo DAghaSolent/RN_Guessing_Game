c9d96ed514021a7b811fb30fbc2f38fe4524d890
DAghaSolent <5aghad30@solent.ac.uk>
2025-03-13 14:27:46 +0000

    Initial styling of main start screen and number input section.

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,11 +1,41 @@
-import { TextInput, View } from "react-native";
+import { TextInput, View, StyleSheet } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
   return (
-    <View>
-      <TextInput />
+    <View style={styles.inputContainer}>
+      <TextInput style={styles.numberInputStyle} maxLength={2} />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
     </View>
   );
 }
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: "#72063c",
+    borderRadius: 8,
+    elevation: 4, // Only for Native Android
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+    //Shadow properties for iOS
+  },
+  numberInputStyle: {
+    height: 50,
+    width: 50,
+    textAlign: 'center',
+    fontsize: 64,
+    borderBottomColor: '#ddb52f',
+    color: '#ddb52f',
+    borderBottomWidth: 2,
+    marginVertical: 8,
+    fontWeight: 'bold'
+  },
+});
