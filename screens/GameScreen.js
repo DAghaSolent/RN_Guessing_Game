ce59a861828d909477d9e12281472f2a70a59dfc
DAghaSolent <5aghad30@solent.ac.uk>
2025-04-02 15:01:03 +0100

    Adding and displaying random number logic

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,11 +1,26 @@
+import { useState } from "react";
 import { Text, View, StyleSheet } from "react-native";
 import Title from "../components/Title";
+import NumberContainer from "../components/NumberContainer";
 
-function GameScreen() {
+function generateRandomBetween(min, max, exclude) {
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+}
+
+function GameScreen({ userNumberGuess }) {
+  const initalGuess = generateRandomBetween(1, 100, userNumberGuess);
+  const [currentGuess, setCurrentGuess] = useState(initalGuess);
+
   return (
     <View style={styles.screen}>
       <Title>Opponent's Guess</Title>
-      {/* Guess */}
+      <NumberContainer>{currentGuess}</NumberContainer>
       <View>
         <Text>Higher or Lower?</Text>
         {/* +- */}
@@ -20,6 +35,6 @@ export default GameScreen;
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    padding: 24
+    padding: 24,
   },
 });
